15a3a102d47f1680ff91ecadb702b431499210f8
Maciej Hyrycz <m.hyrycz@skelia.local>
2021-03-19 00:17:02 +0100

    fetch beers list added.

--- src/components/BeersList.tsx
+++ src/components/BeersList.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../redux/hooks'
 import { selectBeers, updateBeersList } from "../redux/beersSlice"
 
 function BeerList() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
     const dispatch = useAppDispatch()
 
-    const onClickUpdateBeers = () => dispatch(updateBeersList(["ale", "pale", "dark"]))
+    useEffect(() => {
+        fetch("https://api.punkapi.com/v2/beers")
+            .then(res => res.json())
+            .then(
+                (result: any) => {
+                    setIsLoaded(true);
+                    const beersNames = result.map((r: any) => r.name)
+                    dispatch(updateBeersList(beersNames));
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [dispatch])
+
+    const beersList = useAppSelector(selectBeers)
+
     return (
         <div className="App">
             <header className="App-header">
-                <button type="button" onClick={onClickUpdateBeers}>
-                    pour Beers
-                </button>
-                {JSON.stringify(useAppSelector(selectBeers))}
+                {isLoaded ? JSON.stringify(beersList) : "...Loading"}
+                {error}
             </header>
         </div>
     );
