cb863fc3574a80b0e6912a676bcd59bfdc20e7d3
Maciej Hyrycz <m.hyrycz@skelia.local>
2021-03-18 23:14:07 +0100

    Basic store configured with beers list update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useAppSelector, useAppDispatch } from './redux/hooks'
+import { selectBeers, updateBeersList } from "./redux/beersSlice"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useAppDispatch()
+
+    const onClickUpdateBeers = () => dispatch(updateBeersList(["ale", "pale", "dark"]))
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button type="button" onClick={onClickUpdateBeers}>
+                    pour Beers
+                </button>
+                {JSON.stringify(useAppSelector(selectBeers))}
+            </header>
+        </div>
+    );
 }
 
 export default App;
